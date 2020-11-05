e1f793c9c15eb8a149edcb423ae8e96d44f19bc9
nblack0917 <nickablack@gmail.com>
2020-11-05 16:17:43 -0600

    completed working GUI

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-
-let startMove;
-let endMove;
-let numberOfMoves = 0;
 let win = false;
 let stone = null;
-// let stoneOne = document.getElementById("1").id
-// let stoneTwo = document.getElementById("2").id
-// let stoneThree = document.getElementById("3").id
-// let stoneFour = document.getElementById("4").id
-let a = document.getElementById("bottom-row")
-let b = document.getElementById("middle-row")
-let c = document.getElementById("top-row")
+const a = document.getElementById("bottom-row")
+const b = document.getElementById("middle-row")
+const c = document.getElementById("top-row")
+const selectionBox = document.getElementById("selection-box")
+let numOfMoves = document.getElementById("numOfMoves")
+const stoneOne = document.getElementById("1")
+const stoneTwo = document.getElementById("2")
+const stoneThree = document.getElementById("3")
+const stoneFour = document.getElementById("4")
 let currentStoneSize;
 let lastStoneSize;
 let startingSize = parseInt(c.childElementCount)
+let numberOfMoves = 0;
+let stoneSelection = null;
 
 const checkForWin = () => {
   if (a.childElementCount == startingSize || b.childElementCount == startingSize) {
-    alert("You Win!")
+    setTimeout(function() {
+      if (numberOfMoves === 15) {
+        alert("You Win! That's a perfect score!")
+        document.getElementById("resetButton").style.visibility = "visible";
+      } else {
+        alert("You Win! But it can be done with less moves. See if you can do beter.")
+        document.getElementById("resetButton").style.visibility = "visible";
+      }
+    }, 500)
+    win = true;
   }
 }
 
 const selectRow = (row) => {
-  const currentRow = row.getAttribute("data-row")
+  // const currentRow = row.getAttribute("data-row")
   const selectedRow = row.id;
   const selectedRowID = document.getElementById(selectedRow)
   console.log("selected Row " + selectedRow)
-  // console.log("Yay, we clicked an item", row)
-  // console.log("Here is the stone's id: ", row.id)
-  // console.log("Here is the stone's data-size: ", currentRow)
   console.log("There are stones to pickup = " + row.hasChildNodes())
   if (row.hasChildNodes() && !stone) {
     pickUpStone(selectedRow)
@@ -47,6 +53,7 @@ const selectRow = (row) => {
   } else if (stone) {
     dropStone(selectedRow, stone)
     stone = null
+    
   }
 } 
 
@@ -56,6 +63,7 @@ const pickUpStone = (rowID) => {
   console.log("stone picked up")
   const selectedRow = document.getElementById(rowID);
   stone = selectedRow.removeChild(selectedRow.lastElementChild);
+  selectionBox.appendChild(stone)
   currentStoneSize = parseInt(stone.getAttribute('data-size'))
   if (selectedRow.lastElementChild != null) {
     lastStoneSize = parseInt(selectedRow.lastElementChild.getAttribute("data-size"))
@@ -74,63 +82,27 @@ const pickUpStone = (rowID) => {
 
 const dropStone = (rowID, stone) => {
   console.log("stone dropped")
+  numberOfMoves++
+  numOfMoves.innerHTML = `Moves: ${numberOfMoves}`
   document.getElementById(rowID).appendChild(stone)
-  console.log("dropped")
+  // console.log("dropped")
   if (document.getElementById(rowID) === "bottom-row") {
     movePiece()
   }
   currentStoneSize = null;
+ 
   checkForWin();
-  // stone = null
 }
 
-// * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
+const resetBoard = () => {
+  numOfMoves.innerHTML = "Moves: 0"
+  numberOfMoves = 0;
+  a.innerHTML = null
+  b.innerHTML = null
+  c.appendChild(stoneFour)
+  c.appendChild(stoneThree)
+  c.appendChild(stoneTwo)
+  c.appendChild(stoneOne)
+  document.getElementById("resetButton").style.visibility = "hidden";
+}
 
-
-
-// let stacks = {
-//   a: [stoneFour, stoneThree, stoneTwo, stoneOne],
-//   b: [],
-//   c: []
-// };
-
-// let startingSize = stacks.a.length
-
-// const movePiece = (startStack, endStack) => {
-//     let playerMove = startMove.pop();
-//     endMove.push(playerMove)
-//     numberOfMoves++;
-//     if (checkForWin()) {
-//       console.log(`You won in ${numberOfMoves} moves!`)
-//     };
-// }
-
-
-// const isLegal = (x, y) => {
-//   startMove = stacks[x]
-//   endMove = stacks[y]
-//   if (startMove[startMove.length - 1] > endMove[endMove.length - 1]) {
-//     console.log("false")
-//     return false
-//   } else {
-//     console.log("true")
-//     return true
-//   }
-// }
-
-// const checkForWin = () => {
-//   if (stacks.b.length === startingSize || stacks.c.length === startingSize) {
-//     win = true
-//     return true
-//   } else {
-//     return false
-//   }
-// }
-
-// const towersOfHanoi = (startStack, endStack) => {
-//   startMove = stacks[startStack]
-//   endMove = stacks[endStack]
-//   if (isLegal(startStack, endStack)) {
-//     movePiece();
-//   }
-// }
